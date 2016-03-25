a5ceffdd705857e6ee4953f301aeb5a3f953f022
lanou3g <871192135@qq.com>
2016-03-25 11:18:04 +0800

    ll

--- xixixixxi/ViewController.m
+++ xixixixxi/ViewController.m
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"sffgvdg");
+    NSLog(@"sffgvdg");
+    NSLog(@"sffgvdg");
+    NSLog(@"sffgvdg");
+    
 }
 
 - (void)didReceiveMemoryWarning {
